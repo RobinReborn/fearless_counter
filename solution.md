f9aaafe11e30d6ea35b52582ca35497dd8224d56
robinganemccalla@gmail.com <robinganemccalla@gmail.com>
2022-04-10 14:02:42 -0400

    don't set initial value for count, start solution file and allow port customization

---
+++ solution.md
@@ -0,0 +1,5 @@
+To install necessary packages enter ### `npm install`
+
+In order to run enter ### `npm start`
+
+To change the port the code is run on, change the PORT value in the .env file
--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState('');
   function get_count(){
-    fetch('https://api.countapi.xyz/hit/namespace/key')
+    fetch('https://api.countapi.xyz/hit/robinsproject/key')
     .then(response => response.json())
     .then(data => setCount(data.value));
   }
